abc172203650ea440da480ebf9eac43593e18b7e
canhieu2412 <hieucoi2412@gmail.com>
2025-06-26 11:06:58 +0700

    Create Validate.java

---
+++ DOCTOR/Validate.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Validate {
+    private final static Scanner in = new Scanner(System.in);
+
+    public static String checkInputString() {
+        while (true) {
+            String result = in.nextLine().trim();
+            if (!result.isEmpty()) {
+                return result;
+            }
+            System.out.print("Input cannot be empty. Enter again: ");
+        }
+    }
+
+    public static int checkInputIntLimit(int min, int max) {
+        while (true) {
+            try {
+                int result = Integer.parseInt(in.nextLine());
+                if (result >= min && result <= max) {
+                    return result;
+                }
+                System.out.print("Please input number in range [" + min + ", " + max + "]: ");
+            } catch (NumberFormatException e) {
+                System.out.print("Input must be an integer. Enter again: ");
+            }
+        }
+    }
+
+    public static int checkInputInt() {
+        while (true) {
+            try {
+                int result = Integer.parseInt(in.nextLine());
+                if (result >= 0) return result;
+                System.out.print("Availability must be >= 0. Enter again: ");
+            } catch (NumberFormatException e) {
+                System.out.print("Input must be an integer. Enter again: ");
+            }
+        }
+    }
+}
